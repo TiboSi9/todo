f9e96b99ebf56ca87198da5eff0b02db9336c366
TiboSi9 <42150159+TiboSi9@users.noreply.github.com>
2021-03-18 01:38:44 +0800

    1last main

--- static/zdir.js
+++ static/zdir.js
@@ -13,6 +13,9 @@ function msg(){
 	var content = new Array();
 	content[0] = 'Google：<a href = "https://www.google.com.hk/" target = "_blank">https://www.google.com.hk</a>';
 	content[1] = 'Baidu：<a href = "https://www.baidu.com/" target = "_blank">https://www.baidu.com</a>';
+	content[2] = 'Google：<a href = "https://www.google.com.hk/" target = "_blank">https://www.google.com.hk</a>';
+	content[3] = 'Baidu：<a href = "https://www.baidu.com/" target = "_blank">https://www.baidu.com</a>';
+	content[4] = 'Google：<a href = "https://www.google.com.hk/" target = "_blank">https://www.google.com.hk</a>';
 	$("#msg-content").append(content[num]);
 	window.setTimeout("clear_msg()",9800);
 	window.setTimeout("msg()",10000);
