91a3490562ad019a91bb332835acbd0476e90f93
TiboSi9 <42150159+TiboSi9@users.noreply.github.com>
2021-03-17 21:41:29 +0800

    first main

---
+++ config.php
@@ -0,0 +1,47 @@
+<?php
+	$siteinfo = array(
+		"site_name"	=>	"<i class='fa fa-sitemap' aria-hidden='true'></i> Mdo",
+		"title"		=>	"Mdo 目录列表",
+		"keywords"	=>	"Mdo,目录列表,目录索引",
+		"description"	=>	"Mdo 目录列表。"
+	);
+
+	//需要忽略的目录
+	$ignore	= array(
+		".",
+		".git",
+		".user.ini",
+		".htaccess",
+		"favicon.ico",
+		"functions",
+		"config.php",
+		"index.php",
+		"static",
+		"LICENSE",
+		"template",
+		"cache.php",
+		"indexes.php",
+		"Procfile",
+		"composer.json",
+		"vendor",
+		".htaccess.bak",
+		".composer",
+		".heroku",
+		".profile.d",
+		"zdir"
+	);
+	//设置参数
+	$config = array(
+		//thedir为需要读取的目录，如:/data/wwwroot/soft.xiaoz.org，仅将zdir放在子目录的情况下需要配置此项，其它请勿配置此选项
+		"thedir"	=>	'',
+		"allowip"	=>	array(
+			//"0.0.0.0",
+			"::1",
+			"127.0.0.1",
+			"192.168.1."
+		),
+		"username"	=>	"admin",			//用户名
+		"password"	=>	"8@HrSxFaT",		//密码
+		"auth"		=>	FALSE			//是否开启访问验证
+	);	
+?>
---
+++ static/zdir.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	msg();
+});
+
+//清空内容
+function clear_msg(){
+	$("#msg-content").empty();
+}
+function msg(){
+	//生成0-5的随机数
+	var num = parseInt(Math.random()*(4-0+1)+0,10);;
+	
+	var content = new Array();
+	content[0] = 'Google：<a href = "https://www.google.com.hk/" target = "_blank">https://www.google.com.hk</a>';
+	content[1] = 'Baidu：<a href = "https://www.baidu.com/" target = "_blank">https://www.baidu.com</a>';
+	$("#msg-content").append(content[num]);
+	window.setTimeout("clear_msg()",9800);
+	window.setTimeout("msg()",10000);
+}
